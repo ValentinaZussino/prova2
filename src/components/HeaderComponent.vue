42343afe2de0d16891766a551202abcdc30ea8f1
ValentinaZussino <vzussino@protonmail.com>
2022-11-09 12:16:12 +0100

    prova agg componente

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1>{{title}}</h1>
+  </header>
+</template>
+<!-- script -->
+<script>
+export default{
+    name: 'HeaderComponent',
+    data(){
+        return {
+        title: 'Hi!'
+        }
+    }, 
+}
+</script>
+
+<!-- style -->
+<style lang="css">
+header {
+    width: 100%;
+    height: 50px;
+    background-color: black;
+}
+h1 {
+  color: violet;
+}
+</style>
